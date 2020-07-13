5efe23d7bf117b27a96d3813d3677b3a256c7a5f
Nick Schoess <schoessnick@gmail.com>
2020-07-12 21:16:10 -0500

    changed error logic to before the .then so that passport works

--- config/passport.js
+++ config/passport.js
@@ -1,5 +1,6 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
+// const bcrypt = require("bcrypt");
 
 const db = require("../models");
 
@@ -10,11 +11,20 @@ passport.use(
     {
       usernameField: "artistName"
     },
-    (username, password, done) => {
+    (artistName, password, done) => {
       // When a user tries to sign in this code runs
+      console.log("artistName, password: ", artistName, password);
       db.Artist.findOne({
         where: {
-          artistName: username
+          artistName: artistName
+        },
+        function(err, user) {
+          if (err) {
+            return done(err);
+          }
+          if (!user) {
+            return done(null, false, { message: "Incorrect username." });
+          }
         }
       }).then(dbUser => {
         // If there's no user with the given email
@@ -31,6 +41,14 @@ passport.use(
         }
         // If none of the above, return the user
         return done(null, dbUser);
+        // bcrypt.compare(password, dbUser.password, function(err, res) {
+        //   if(err) {
+        //     throw err;
+        //   }
+        //   if(res){
+        //     return done(null,dbUser);
+        //   }
+        // })
       });
     }
   )
