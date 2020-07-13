7e5731d07f29053f5291a2863361caefb0a0c131
Christian Gonzalez <christian.gonzalez.2633@gmail.com>
2020-07-12 21:32:05 -0500

    added handlebars

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const session = require("express-session");
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
+// Set Handlebars.
+const exphbs = require("express-handlebars");
+ 
 
 // Setting up port and requiring models for syncing
 const PORT = process.env.PORT || 8080;
@@ -13,6 +16,9 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+// Adding Handlebars
+app.engine("handlebars", exphbs({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
 // We need to use sessions to keep track of our user's login status
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
